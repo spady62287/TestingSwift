2d656c1a67526ef8bf2c463f41ba604ba3e4d669
Daniel Spady <danielspady@gmail.com>
2021-02-03 11:01:23 -0500

    Initial Commit

---
+++ TestingSwift/TestingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSwiftApp.swift
+//  TestingSwift
+//
+//  Created by Daniel Spady on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct TestingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
