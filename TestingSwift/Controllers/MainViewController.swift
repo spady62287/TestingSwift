4add5272b229a8d4054a2deb3027f4a98e1190e4
Daniel Spady <danielspady@gmail.com>
2021-02-24 09:38:19 -0500

    Build out User Interface for Testing
    Add a storyboard
    Add UI elements to Storyboard
    Link UI elements to View Controller
    Assign base functionality to Controller

---
+++ TestingSwift/Controllers/MainViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainViewController.swift
+//  TestingSwift
+//
+//  Created by Daniel Spady on 2021-02-24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    @IBAction func textDidChange(_ sender: UITextField) {
+        label.text = textField.text
+    }
+    
+    
+    @IBAction func sliderDragged(_ sender: UISlider) {
+        progress.progress = 1 - slider.value
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        title = segmentControl.titleForSegment(at: segmentControl.selectedSegmentIndex)
+    }
+    
+    
+    @IBAction func colorSelected(_ sender: UIButton) {
+        let alert = UIAlertController(title: sender.titleLabel?.text, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
